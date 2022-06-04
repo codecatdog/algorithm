f6dd968175cff555e789b8f2da36d57f1da54e87
codecatdog <1113231244@qq.com>
2022-06-04 20:47:15 +0800

    扩展1：贪心算法

--- 剑指offer/20. 股票的最大利润.js
+++ 剑指offer/20. 股票的最大利润.js
@@ -33,4 +33,32 @@ var maxProfit = function(prices) {
     return res;
 };
 
-console.log(maxProfit([7,6,4,3,1]));
+
+/**
+ * 扩展1：给你一个整数数组 prices ，其中 prices[i] 表示某支股票第 i 天的价格。
+ * 
+ * 在每一天，你可以决定是否购买和/或出售股票。你在任何时候 最多 只能持有 一股 股票。
+ * 你也可以先购买，然后在 同一天 出售。
+ * 返回 你能获得的 最大 利润 。
+ * 
+ * 输入：prices = [7,1,5,3,6,4]
+ * 输出：7
+ * 解释：在第 2 天（股票价格 = 1）的时候买入，在第 3 天（股票价格 = 5）的时候卖出, 
+ * 这笔交易所能获得利润 = 5 - 1 = 4 。
+ * 随后，在第 4 天（股票价格 = 3）的时候买入，在第 5 天（股票价格 = 6）的时候卖出, 
+ * 这笔交易所能获得利润 = 6 - 3 = 3 。
+ * 总利润为 4 + 3 = 7 。
+ * 
+ * 使用贪心算法 本题只要求最大利润，其实就是求出数组中含有的 子升序列 的和 (1, 5) (3, 6)
+ * @param {number[]} prices
+ * @return {number}
+ */
+ var maxProfit1 = function(prices) {
+    let res = 0;
+    let n = prices.length;
+    for (let i = 1; i < n; i ++) {
+        res += Math.max(0, prices[i] - prices[i - 1]);
+    }
+    return res;
+
+};
