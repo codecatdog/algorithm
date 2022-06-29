4b0b5b5c9e359a6136b4bfbb3ccd08e6c8a33e28
ling-zer <1113231244@qq.com>
2022-06-29 21:30:46 +0800

    自底向上层序遍历

---
+++ 深度、广度优先搜索/2. 层序遍历二叉树Ⅱ.js
@@ -0,0 +1,34 @@
+/**
+ * 给你二叉树的根节点 root，返回其节点值 自底向上 的层序遍历。 
+ * （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+const levelOrderBottom = function(root) {
+    if(!root) return [];
+    const res = []; // 结果
+    const queue = [root]; // 队列，用于暂存节点
+    while(queue.length) {
+        const temp = [];
+        const len = queue.length;
+        for(let i = 0; i < len; i++) {
+            const node = queue.shift(); // 节点出队
+            temp.push(node.val);
+            if(node.left) queue.push(node.left);
+            if(node.right) queue.push(node.right);
+        }
+        res.unshift(temp);
+    }
+    return res;
+};
