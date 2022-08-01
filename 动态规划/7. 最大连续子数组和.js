1ed2347faed132f477f53cfd9c9856dc636e3ada
ling-zer <1113231244@qq.com>
2022-08-01 20:54:49 +0800

    连续子数组最大和

--- 动态规划/7. 最大连续子数组和.js
+++ 动态规划/7. 最大连续子数组和.js
@@ -18,3 +18,35 @@ var maxSubArray = function(nums) {
     }
     return maxSum;
 };
+
+/**
+ * 扩展： 返回具有最大和的连续子数组
+ * @param {number[]} nums 
+ * @return {number[]}
+ */
+var maxSumArray = function(nums) {
+    if(nums.length === 0) return nums;
+    const dp = []; // dp[i] 表示以nums[i]结尾的 最大和连续子数组
+    dp[0] = nums[0];
+    let maxSum = dp[0]; // 记录 连续子数组的 最大和
+    let right = 0; // 记录最大和的连续子数组的 结束坐标
+    let dpLen = 1; // 记录以nums[i]结尾的 最大和 连续子数组的长度
+    let len = 1; // 记录 具有最大和的连续子数组 的长度
+    for(let i = 1; i < nums.length; i++) { // [-2,1,-3,4,-1,2,1,-5,4]
+        if(dp[i - 1] > 0) {
+            dp[i] = dp[i - 1] + nums[i];
+            dpLen ++;
+        } else {
+            dp[i] = nums[i];
+            dpLen = 1;
+        }
+        if(dp[i] > maxSum) {
+            right = i;
+            len = dpLen;
+            maxSum = dp[i];
+        }
+    }
+    return nums.slice(right - len + 1, right + 1);
+}
+
+console.log(maxSumArray([5,4,-1,7,8]))
